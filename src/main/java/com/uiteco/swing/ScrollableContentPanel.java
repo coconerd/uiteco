cf1b672159503cf0506c41df12eaac81faa5778c
ducmint864 <nddminh2021@gmail.com>
2024-04-14 22:12:22 +0700

    misc

--- src/main/java/com/uiteco/swing/ScrollableContentPanel.java
+++ src/main/java/com/uiteco/swing/ScrollableContentPanel.java
@@ -1,7 +1,6 @@
 package com.uiteco.swing;
 
 import java.awt.BorderLayout;
-import javax.swing.JScrollPane;
 import javax.swing.ScrollPaneConstants;
 import com.raven.scroll.ScrollPaneWin11;
 
@@ -10,13 +9,11 @@ import com.raven.scroll.ScrollPaneWin11;
  * @author nddmi
  */
 public class ScrollableContentPanel extends DynamicPanel {
-//    private final JScrollPane scrollPane;
     private final ScrollPaneWin11 scrollPane;
     
     public ScrollableContentPanel() {
         super();
         this.setLayout(new java.awt.BorderLayout());
-//        this.scrollPane = new JScrollPane();
         this.scrollPane= new ScrollPaneWin11();
         this.scrollPane.setViewportView(this.pageHolder);
         this.scrollPane.setBorder(null);
