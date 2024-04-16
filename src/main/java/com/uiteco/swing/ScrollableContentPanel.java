2d0aacf139cec32b38160fc8eed5f2bb33ae876c
ducmint864 <nddminh2021@gmail.com>
2024-04-16 13:46:33 +0700

    Update ContentPanel.java, DynamicPanel.java, and ScrollableContentPanel.java: - Move the code to add padding from ScrollableContentPanel class into DynamicPanel class - Refactor ScrollableContentPanel class - Refactor ContentPanel class

--- src/main/java/com/uiteco/swing/ScrollableContentPanel.java
+++ src/main/java/com/uiteco/swing/ScrollableContentPanel.java
@@ -3,11 +3,6 @@ package com.uiteco.swing;
 import java.awt.BorderLayout;
 import javax.swing.ScrollPaneConstants;
 import com.raven.scroll.ScrollPaneWin11;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
-import java.awt.Component;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 
 /**
  *
@@ -15,10 +10,19 @@ import javax.swing.border.EmptyBorder;
  */
 public class ScrollableContentPanel extends DynamicPanel {
 
-    private final ScrollPaneWin11 scrollPane;
+    private ScrollPaneWin11 scrollPane;
 
     public ScrollableContentPanel() {
         super();
+        _init();
+    }
+
+    public ScrollableContentPanel(int innerPaddingSize) {
+        super(innerPaddingSize);
+        _init();
+    }
+
+    private void _init() {
         this.setLayout(new java.awt.BorderLayout());
         this.scrollPane = new ScrollPaneWin11();
         this.scrollPane.setViewportView(this.pageHolder);
@@ -26,26 +30,5 @@ public class ScrollableContentPanel extends DynamicPanel {
         this.scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         this.add(scrollPane, BorderLayout.CENTER);
         this.add(scrollPane);
-
-        scrollPane.getViewport().addComponentListener(new ComponentAdapter() {
-            @Override
-            public void componentResized(ComponentEvent e) {
-                // This function will be invoked after all child components are loaded
-                for (Component comp : pageHolder.getComponents()) {
-                    if (!(comp instanceof JPanel)) {
-                        continue;
-                    }
-                    
-                    for (Component subcomp : ((JPanel)comp).getComponents()) {
-                        if (!(subcomp instanceof JPanel))
-                            continue;
-                        
-                        ((JPanel)subcomp).setBorder(new EmptyBorder(0, 0, 0, 20)); // Add right border
-                    }
-                }
-            }
-        });
-
     }
-
 }
