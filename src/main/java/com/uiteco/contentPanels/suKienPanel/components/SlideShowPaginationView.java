a0f0887c1297b8a353ed5d7a044b4e9d2b34f379
ducmint864 <nddminh2021@gmail.com>
2024-04-22 22:54:42 +0700

    Fix SlideShowPaginationView not visible

--- src/main/java/com/uiteco/contentPanels/suKienPanel/components/SlideShowPaginationView.java
+++ src/main/java/com/uiteco/contentPanels/suKienPanel/components/SlideShowPaginationView.java
@@ -14,11 +14,17 @@ import java.awt.AlphaComposite;
 import java.awt.Color;
 import javax.swing.border.EmptyBorder;
 import java.awt.Cursor;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import net.miginfocom.swing.MigLayout;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.FlowLayout;
+import javax.swing.SwingConstants;
 
 /**
  *
@@ -26,34 +32,31 @@ import java.beans.PropertyChangeEvent;
  */
 public class SlideShowPaginationView extends JComponent implements PropertyChangeListener {
 
-    private int animationFraction;
+//    private int animationFraction;
     private SlideShowModel slideShowModel;
     private MigLayout layout;
+    private ImageIcon activeIcon;
+    private ImageIcon inactiveIcon;
+
+    public static String DEFAULT_ACTIVE_ICON_NAME = "/icons8-light-dot-26.png";
+    public static String DEFAULT_INACTIVE_ICON_NAME = "/icons8-dark-dot-26.png";
 
     public SlideShowPaginationView() {
+        setSlideShowModel(new SlideShowModel());
         _init();
     }
 
     public SlideShowPaginationView(SlideShowModel slideShowModel) {
         setSlideShowModel(slideShowModel);
         _init();
+    }
 
-        slideShowModel.addPropertyChangeListener(this);
-        
-        removeAll();
-        for (int i = 0; i < slideShowModel.getPageCount(); i++) {
-            final int page = i + 1;
-            SlideShowButton button = new SlideShowButton();
-            button.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    slideShowModel.switchPage(page);
-                }
-            });
-            add(button);
-        };
-        this.repaint();
-        this.revalidate();
+    public ImageIcon getActiveIcon() {
+        return activeIcon;
+    }
+
+    public ImageIcon getInactiveIcon() {
+        return inactiveIcon;
     }
 
     public SlideShowModel getSlideShowModel() {
@@ -63,10 +66,10 @@ public class SlideShowPaginationView extends JComponent implements PropertyChang
     public MigLayout getLayout() {
         return layout;
     }
-
-    public int getAnimationFraction() {
-        return this.animationFraction;
-    }
+//
+//    public int getAnimationFraction() {
+//        return this.animationFraction;
+//    }
 
     public void setSlideShowModel(SlideShowModel slideShowModel) {
         this.slideShowModel = slideShowModel;
@@ -76,8 +79,15 @@ public class SlideShowPaginationView extends JComponent implements PropertyChang
         this.layout = layout;
     }
 
-    public void setAnimationFraction(int animationFraction) {
-        this.animationFraction = animationFraction;
+//    public void setAnimationFraction(int animationFraction) {
+//        this.animationFraction = animationFraction;
+//    }
+    public void setActiveIcon(ImageIcon activeIcon) {
+        this.activeIcon = activeIcon;
+    }
+
+    public void setInactiveIcon(ImageIcon inactiveIcon) {
+        this.inactiveIcon = inactiveIcon;
     }
 
     @Override
@@ -85,59 +95,87 @@ public class SlideShowPaginationView extends JComponent implements PropertyChang
         if (evt.getPropertyName().equals("currentPage")) {
             int oldIndex = (Integer) evt.getOldValue();
             int newIndex = (Integer) evt.getNewValue();
-            int alpha = getAnimationFraction();
-            SlideShowButton buttonOut = (SlideShowButton) getComponent(oldIndex);
-            SlideShowButton buttonIn = (SlideShowButton) getComponent(newIndex);
-            buttonIn.setAlpha(alpha);
-            buttonOut.setAlpha(1f - alpha);
+//            int alpha = getAnimationFraction();
+            JLabel buttonOut = (JLabel) getComponent(oldIndex);
+            JLabel buttonIn = (JLabel) getComponent(newIndex);
+            buttonOut.setIcon(getInactiveIcon());
+            buttonIn.setIcon(getActiveIcon());
         }
     }
 
     private void _init() {
-        setLayout(new MigLayout("inset 15", "[center]10[center]"));
+//        setLayout(new MigLayout("inset 15", "[center]10[center]"));
+        slideShowModel.addPropertyChangeListener(this);
+        
+        setLayout(new FlowLayout(FlowLayout.CENTER));
+        setActiveIcon(new ImageIcon(getClass().getResource(DEFAULT_ACTIVE_ICON_NAME)));
+        setInactiveIcon(new ImageIcon(getClass().getResource(DEFAULT_INACTIVE_ICON_NAME)));
+
+        removeAll();
+        for (int i = 0; i < slideShowModel.getPageCount(); i++) {
+            final int page = i + 1;
+            JLabel pageButton = new JLabel();
+            pageButton.setIcon(getInactiveIcon());
+            pageButton.setHorizontalAlignment(SwingConstants.CENTER);
+
+            pageButton.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    slideShowModel.switchPage(page);
+                }
+            });
+            add(pageButton);
+            if (i == slideShowModel.getPageCount() - 1) {
+                setSize(pageButton.getWidth() * (i + 1), getHeight());
+            }
+        };
+        repaint();
+        revalidate();
+
     }
-
-    public class SlideShowButton extends JButton {
-
-        private float alpha;
-
-        public SlideShowButton() {
-            setContentAreaFilled(false);
-            setBorder(new EmptyBorder(5, 5, 5, 5));
-            setBackground(Color.WHITE);
-            setCursor(new Cursor(Cursor.HAND_CURSOR));
-//            addActionListener(new ActionListener() {
-//                @Override
-//                public void actionPerformed(ActionEvent ae) {
-//                    
-//                }
-//            });
-
-        }
-
-        public float getAlpha() {
-            return alpha;
-        }
-
-        public void setAlpha(float alpha) {
-            this.alpha = alpha;
-            this.repaint();
-        }
-
-        @Override
-        public void paint(Graphics grphcs) {
-            super.paint(grphcs);
-            int width = getWidth();
-            int height = getHeight();
-            Graphics2D g2 = (Graphics2D) grphcs.create();
-            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-            g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.3f));
-            g2.setColor(getBackground());
-            g2.fillOval(0, 0, width, height);
-            g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
-            g2.setColor(new Color(11, 124, 173));
-            g2.fillOval(0, 0, width, height);
-            g2.dispose();
-        }
-    }
+//
+//    public class SlideShowButton extends JButton {
+//
+//        private float alpha;
+//
+//        public SlideShowButton() {
+//            setContentAreaFilled(false);
+//            setBorder(new EmptyBorder(5, 5, 5, 5));
+//            setBackground(Color.WHITE);
+//            setCursor(new Cursor(Cursor.HAND_CURSOR));
+////            addActionListener(new ActionListener() {
+////                @Override
+////                public void actionPerformed(ActionEvent ae) {
+////                    
+////                }    public class SlideShowButton extends JButton {
+//
+////            });
+//
+//        }
+//
+//        public float getAlpha() {
+//            return alpha;
+//        }
+//
+//        public void setAlpha(float alpha) {
+//            this.alpha = alpha;
+//            this.repaint();
+//        }
+//
+//        @Override
+//        public void paint(Graphics grphcs) {
+//            super.paint(grphcs);
+//            int width = getWidth();
+//            int height = getHeight();
+//            Graphics2D g2 = (Graphics2D) grphcs.create();
+//            g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+//            g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.3f));
+//            g2.setColor(getBackground());
+//            g2.fillOval(0, 0, width, height);
+//            g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
+//            g2.setColor(new Color(11, 124, 173));
+//            g2.fillOval(0, 0, width, height);
+//            g2.dispose();
+//        }
+//    }
 }
