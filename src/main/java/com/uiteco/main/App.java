03da19f62a197946c7ed0476db3ad246cfd3e5c4
Phan Thành Công <22520170@gm.uit.edu.vn>
2024-05-10 15:06:39 +0700

    - Enable login feature (App.java) - Save username and email to Session after login (AuthModel.java) - Change visibility of setUsername() and setEmail() in Session.java from private to public (Session.java)

--- src/main/java/com/uiteco/main/App.java
+++ src/main/java/com/uiteco/main/App.java
@@ -11,7 +11,7 @@ import java.beans.PropertyChangeListener;
  */
 
 public class App {
-
+    private static Session session; 
     private static MainFrame mainFrame;
     private static boolean running = false;
 
@@ -23,44 +23,46 @@ public class App {
     public static boolean isRunning() {
         return running;
     }
+    
+    public static Session getSession() {
+        return session;
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-
+        
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-//                AuthView authView = null;
-//
-//                Session session = new Session();
-//                if (session.isPermitted()) {
-//                    new MainFrame().setVisible(true);
-//                } else {
-//                    authView = new AuthView();
-//                    authView.getAuthModel().addPropertyChangeListener(session);
-//                    authView.setVisible(true);
-//                }
+                AuthView authView = null;
 
-//                final AuthView af = authView;
-//                session.addPropertyChangeListener(new PropertyChangeListener() {
-//                    @Override
-//                    public void propertyChange(PropertyChangeEvent evt) {
-//                        if (evt.getPropertyName().equals("permitted")) {
-//                            if (session.isPermitted()) {
-//                                if (af != null) {
-//                                    af.setVisible(false);
-//                                    af.dispose();
-//                                }
-//                                mainFrame = new MainFrame();
-//                                mainFrame.setVisible(true);
-//                            }
-//                        }
-//                    }
-//                });
-                mainFrame = new MainFrame();
-                mainFrame.setVisible(true);
+                session = new Session();
+                if (session.isPermitted()) {
+                    new MainFrame().setVisible(true);
+                } else {
+                    authView = new AuthView();
+                    authView.getAuthModel().addPropertyChangeListener(session);
+                    authView.setVisible(true);
+                }
+
+                final AuthView af = authView;
+                session.addPropertyChangeListener(new PropertyChangeListener() {
+                    @Override
+                    public void propertyChange(PropertyChangeEvent evt) {
+                        if (evt.getPropertyName().equals("permitted")) {
+                            if (session.isPermitted()) {
+                                if (af != null) {
+                                    af.setVisible(false);
+                                    af.dispose();
+                                }
+                                mainFrame = new MainFrame();
+                                mainFrame.setVisible(true);
+                            }
+                        }
+                    }
+                });
 
                 running = true;
             }
