84c913863834544bd393a056745e799889748f50
ducmint864 <nddminh2021@gmail.com>
2024-05-14 15:44:12 +0700

    Add UI buttons for sorting feature in TagsAndSort pane

--- src/main/java/com/uiteco/ofSuKienPanel/tagsAndSort/TagsAndSort.java
+++ src/main/java/com/uiteco/ofSuKienPanel/tagsAndSort/TagsAndSort.java
@@ -23,6 +23,7 @@ import java.util.TimerTask;
 import java.util.Timer;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JComponent;
 
 /**
  *
@@ -161,6 +162,10 @@ public class TagsAndSort extends RoundedPanel implements PropertyChangeListener 
         tagLabel = new javax.swing.JLabel();
         applyBtn = new com.uiteco.components.RoundedGradientPanel();
         applyLabel = new javax.swing.JLabel();
+        hotBtn = new com.uiteco.components.RoundedPanel();
+        hotLabel = new javax.swing.JLabel();
+        newBtn = new com.uiteco.components.RoundedPanel();
+        newLabel = new javax.swing.JLabel();
 
         setBackground(new java.awt.Color(255, 255, 255));
         setRoundBottomLeft(50);
@@ -169,16 +174,15 @@ public class TagsAndSort extends RoundedPanel implements PropertyChangeListener 
         setRoundTopRight(50);
         setLayout(new java.awt.GridBagLayout());
 
-        sortLabel.setFont(new java.awt.Font("Circular Std Black", 0, 14)); // NOI18N
+        sortLabel.setFont(new java.awt.Font("Arial", 1, 15)); // NOI18N
         sortLabel.setForeground(new java.awt.Color(51, 51, 51));
-        sortLabel.setText("Sap xep:");
+        sortLabel.setText("Sắp xếp:");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 2;
         gridBagConstraints.gridy = 0;
         gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
-        gridBagConstraints.weightx = 0.8;
         gridBagConstraints.weighty = 0.3;
-        gridBagConstraints.insets = new java.awt.Insets(0, 0, 0, 30);
+        gridBagConstraints.insets = new java.awt.Insets(0, 0, 0, 10);
         add(sortLabel, gridBagConstraints);
 
         comboBoxMulti.setPreferredSize(new java.awt.Dimension(80, 35));
@@ -186,11 +190,12 @@ public class TagsAndSort extends RoundedPanel implements PropertyChangeListener 
         gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 0;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
-        gridBagConstraints.weightx = 0.2;
+        gridBagConstraints.weightx = 0.1;
         gridBagConstraints.weighty = 0.3;
+        gridBagConstraints.insets = new java.awt.Insets(0, 0, 0, 50);
         add(comboBoxMulti, gridBagConstraints);
 
-        tagLabel.setFont(new java.awt.Font("Circular Std Black", 0, 14)); // NOI18N
+        tagLabel.setFont(new java.awt.Font("Arial", 1, 15)); // NOI18N
         tagLabel.setForeground(new java.awt.Color(51, 51, 51));
         tagLabel.setText("Tag:");
         gridBagConstraints = new java.awt.GridBagConstraints();
@@ -238,12 +243,88 @@ public class TagsAndSort extends RoundedPanel implements PropertyChangeListener 
         applyBtn.add(applyLabel);
 
         gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 3;
+        gridBagConstraints.gridx = 5;
         gridBagConstraints.gridy = 0;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.weighty = 0.3;
         gridBagConstraints.insets = new java.awt.Insets(0, 0, 0, 30);
         add(applyBtn, gridBagConstraints);
+
+        hotBtn.setBackground(new java.awt.Color(240, 242, 245));
+        hotBtn.setPreferredSize(new java.awt.Dimension(90, 35));
+        hotBtn.setRoundBottomLeft(15);
+        hotBtn.setRoundBottomRight(15);
+        hotBtn.setRoundTopLeft(15);
+        hotBtn.setRoundTopRight(15);
+        hotBtn.setLayout(new java.awt.GridLayout());
+
+        hotLabel.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        hotLabel.setForeground(new java.awt.Color(5, 5, 5));
+        hotLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        hotLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons8-flame-24.png"))); // NOI18N
+        hotLabel.setText("Hot");
+        hotLabel.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                hotLabelMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                hotLabelMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                hotLabelMouseExited(evt);
+            }
+        });
+        hotBtn.add(hotLabel);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 3;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.weighty = 0.3;
+        gridBagConstraints.insets = new java.awt.Insets(0, 0, 0, 15);
+        add(hotBtn, gridBagConstraints);
+
+        newBtn.setBackground(new java.awt.Color(240, 242, 245));
+        newBtn.setPreferredSize(new java.awt.Dimension(100, 35));
+        newBtn.setRoundBottomLeft(15);
+        newBtn.setRoundBottomRight(15);
+        newBtn.setRoundTopLeft(15);
+        newBtn.setRoundTopRight(15);
+        newBtn.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                newBtnMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                newBtnMouseExited(evt);
+            }
+        });
+        newBtn.setLayout(new java.awt.GridLayout());
+
+        newLabel.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        newLabel.setForeground(new java.awt.Color(5, 5, 5));
+        newLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        newLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons8-bell-24.png"))); // NOI18N
+        newLabel.setText("Mới nhất");
+        newLabel.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                newLabelMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                newLabelMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                newLabelMouseExited(evt);
+            }
+        });
+        newBtn.add(newLabel);
+
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 4;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.weighty = 0.3;
+        gridBagConstraints.insets = new java.awt.Insets(0, 0, 0, 50);
+        add(newBtn, gridBagConstraints);
     }// </editor-fold>//GEN-END:initComponents
 
     private void applyBtnMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_applyBtnMouseEntered
@@ -278,13 +359,14 @@ public class TagsAndSort extends RoundedPanel implements PropertyChangeListener 
                 }
             };
 
-            long delay = (long)(1.5 * 1000); // milliseconds
+            long delay = (long) (1.5 * 1000); // milliseconds
             new Timer().schedule(undo, delay);
 
         } catch (NullSuKienViewException e) {
             e.printStackTrace();
         }
     }//GEN-LAST:event_applyLabelMouseClicked
+
 
     private void applyLabelMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_applyLabelMouseEntered
         // TODO add your handling code here:
@@ -301,13 +383,83 @@ public class TagsAndSort extends RoundedPanel implements PropertyChangeListener 
         }
     }//GEN-LAST:event_applyLabelMouseExited
 
+    private void hotLabelMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_hotLabelMouseEntered
+        // TODO add your handling code here:
+        if (chosenBtn != hotBtn) {
+            hotLabel.setForeground(hotLabel.getForeground().darker());
+            hotBtn.setBackground(new Color(224, 228, 234));
+        }
+    }//GEN-LAST:event_hotLabelMouseEntered
+
+    private void hotLabelMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_hotLabelMouseExited
+        // TODO add your handling code here:
+        if (chosenBtn != hotBtn) {
+            hotLabel.setForeground(hotLabel.getForeground().brighter());
+            hotBtn.setBackground(new Color(240, 242, 245));
+        }
+    }//GEN-LAST:event_hotLabelMouseExited
+
+    private void newBtnMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_newBtnMouseEntered
+        // TODO add your handling code here:
+    }//GEN-LAST:event_newBtnMouseEntered
+
+    private void newBtnMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_newBtnMouseExited
+        // TODO add your handling code here:
+    }//GEN-LAST:event_newBtnMouseExited
+
+    private void hotLabelMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_hotLabelMouseClicked
+        // TODO add your handling code here:
+        if (chosenBtn != hotBtn) {
+            chosenBtn = hotBtn;
+
+            hotLabel.setForeground(hotLabel.getForeground().darker());
+            hotBtn.setBackground(new Color(204, 204, 204));
+
+            newLabel.setForeground(newLabel.getForeground().brighter());
+            newBtn.setBackground(new Color(240, 242, 245));
+        }
+    }//GEN-LAST:event_hotLabelMouseClicked
+
+    private void newLabelMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_newLabelMouseEntered
+        // TODO add your handling code here:
+        if (chosenBtn != newBtn) {
+            newLabel.setForeground(newLabel.getForeground().darker());
+            newBtn.setBackground(new Color(224, 228, 234));
+        }
+    }//GEN-LAST:event_newLabelMouseEntered
+
+    private void newLabelMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_newLabelMouseExited
+        // TODO add your handling code here:
+        if (chosenBtn != newBtn) {
+            newLabel.setForeground(newLabel.getForeground().brighter());
+            newBtn.setBackground(new Color(240, 242, 245));
+        }
+    }//GEN-LAST:event_newLabelMouseExited
+
+    private void newLabelMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_newLabelMouseClicked
+        // TODO add your handling code here:
+        if (chosenBtn != newBtn) {
+            chosenBtn = newBtn;
+            newLabel.setForeground(newLabel.getForeground().darker());
+            newBtn.setBackground(new Color(204, 204, 204));
+
+            hotLabel.setForeground(hotLabel.getForeground().brighter());
+            hotBtn.setBackground(new Color(240, 242, 245));
+        }
+    }//GEN-LAST:event_newLabelMouseClicked
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private com.uiteco.components.RoundedGradientPanel applyBtn;
     private javax.swing.JLabel applyLabel;
     private com.uiteco.components.comboBox.ComboBoxMultiSelectionLimitted comboBoxMulti;
+    private com.uiteco.components.RoundedPanel hotBtn;
+    private javax.swing.JLabel hotLabel;
+    private com.uiteco.components.RoundedPanel newBtn;
+    private javax.swing.JLabel newLabel;
     private javax.swing.JLabel sortLabel;
     private javax.swing.JLabel tagLabel;
     // End of variables declaration//GEN-END:variables
     private boolean displayingAnimation;
+    private Component chosenBtn;
 }
