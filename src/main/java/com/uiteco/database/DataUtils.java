1b1debd40d48f3756d9b6300d2c646e3d1f81822
ducmint864 <nddminh2021@gmail.com>
2024-05-19 20:46:20 +0700

    Add utility class com.uiteco.database.DataUtils to provide helper functions related to data types manipulation when working with database Conflicts: 	src/main/java/com/uiteco/ofSuKienPanel/SuKienDAO.java (changes in this file from branch ducminh discarded for safety)

---
+++ src/main/java/com/uiteco/database/DataUtils.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.uiteco.database;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author nddmi
+ */
+public class DataUtils {
+
+    public static byte[] convertImageIconToBytes(ImageIcon imageIcon) throws IOException {
+        // Get the image from the ImageIcon
+        Image image = imageIcon.getImage();
+
+        // Convert the Image to BufferedImage
+        BufferedImage bufferedImage = new BufferedImage(image.getWidth(null), image.getHeight(null), BufferedImage.TYPE_INT_RGB);
+        bufferedImage.getGraphics().drawImage(image, 0, 0, null);
+
+        // Write the BufferedImage to ByteArrayOutputStream
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(bufferedImage, "jpg", baos);
+
+        // Convert the ByteArrayOutputStream to byte[]
+        return baos.toByteArray();
+    }
+
+    public static ImageIcon convertBytesToImageIcon(byte[] bytes) throws IOException {
+        ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+        Image image = ImageIO.read(bais);
+        return new ImageIcon(image);
+    }
+}
