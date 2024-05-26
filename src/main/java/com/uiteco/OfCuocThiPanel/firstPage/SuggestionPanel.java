67c1f2c1e70431adb2d379e506beac72c476173c
cahoivuotthac <thuyhienphanthi2004@gmail.com>
2024-05-27 02:32:12 +0700

    checkpoint

--- src/main/java/com/uiteco/OfCuocThiPanel/firstPage/SuggestionPanel.java
+++ src/main/java/com/uiteco/OfCuocThiPanel/firstPage/SuggestionPanel.java
@@ -25,8 +25,14 @@ public class SuggestionPanel extends javax.swing.JPanel {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+        java.awt.GridBagConstraints gridBagConstraints;
 
         roundedGradientPanel1 = new com.uiteco.components.RoundedGradientPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+
+        setOpaque(false);
 
         roundedGradientPanel1.setColor1(new java.awt.Color(255, 254, 255));
         roundedGradientPanel1.setColor2(new java.awt.Color(215, 255, 254));
@@ -34,17 +40,32 @@ public class SuggestionPanel extends javax.swing.JPanel {
         roundedGradientPanel1.setRoundBottomRight(45);
         roundedGradientPanel1.setRoundTopLeft(45);
         roundedGradientPanel1.setRoundTopRight(45);
+        roundedGradientPanel1.setLayout(new java.awt.GridBagLayout());
 
-        javax.swing.GroupLayout roundedGradientPanel1Layout = new javax.swing.GroupLayout(roundedGradientPanel1);
-        roundedGradientPanel1.setLayout(roundedGradientPanel1Layout);
-        roundedGradientPanel1Layout.setHorizontalGroup(
-            roundedGradientPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1004, Short.MAX_VALUE)
-        );
-        roundedGradientPanel1Layout.setVerticalGroup(
-            roundedGradientPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 767, Short.MAX_VALUE)
-        );
+        jLabel1.setFont(new java.awt.Font("Merriweather", 0, 30)); // NOI18N
+        jLabel1.setText("Competitions suggestion to you");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTH;
+        gridBagConstraints.insets = new java.awt.Insets(6, 0, 25, 0);
+        roundedGradientPanel1.add(jLabel1, gridBagConstraints);
+
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/2_files/icons8-light-45.png"))); // NOI18N
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.VERTICAL;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTH;
+        gridBagConstraints.insets = new java.awt.Insets(0, 0, 0, 10);
+        roundedGradientPanel1.add(jLabel3, gridBagConstraints);
+
+        jLabel4.setFont(new java.awt.Font("Merriweather", 2, 22)); // NOI18N
+        jLabel4.setText("Coming soon ...");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 1;
+        roundedGradientPanel1.add(jLabel4, gridBagConstraints);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -60,6 +81,9 @@ public class SuggestionPanel extends javax.swing.JPanel {
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private com.uiteco.components.RoundedGradientPanel roundedGradientPanel1;
     // End of variables declaration//GEN-END:variables
 }
